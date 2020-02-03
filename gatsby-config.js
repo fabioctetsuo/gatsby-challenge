88908a7c253dbd7970d6aa2388d0b89e95f55bfe
Fabio Tetsuo <fabioctetsuo@gmail.com>
2020-02-03 19:22:20 -0300

    fix: removing manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,21 +32,9 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: `${__dirname}/src/utils/typography`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
